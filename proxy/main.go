64962ab587cc930d2b6832d2af19de84189ed7c4
CaleHH <caleihhmy@gmail.com>
2022-09-13 16:07:40 +0800

    add snapshot

--- proxy/main.go
+++ proxy/main.go
@@ -8,9 +8,11 @@ import (
 	"io/ioutil"
 	"os"
 	"os/signal"
+	cluster_client "proxy/cluster-client"
 	db2 "proxy/db"
 	"proxy/log"
 	"proxy/restful"
+	"proxy/snapshot"
 	"syscall"
 )
 
@@ -56,6 +58,22 @@ var (
 		Usage: "config path",
 		Value: "./config.yml",
 	}
+	clusterUrlFlag = cli.StringFlag{
+		Name:  "cluster_url",
+		Usage: "cluster url",
+	}
+	clusterPass = cli.StringFlag{
+		Name:  "cluster_pass",
+		Usage: "cluster pass",
+	}
+	clusterName = cli.StringFlag{
+		Name:  "cluster_name",
+		Usage: "cluster name",
+	}
+	headlessUrl = cli.StringFlag{
+		Name:  "headless_url",
+		Usage: "cluster name",
+	}
 )
 
 func init() {
@@ -94,8 +112,11 @@ var commandFile = cli.Command{
 	Name:  "file",
 	Usage: "start file server",
 	Flags: []cli.Flag{
-		dbIPFlag,
 		portFlag,
+		clusterPass,
+		clusterUrlFlag,
+		clusterName,
+		headlessUrl,
 	},
 	Action: FileServer,
 }
@@ -159,8 +180,25 @@ func loadConfig(ctx *cli.Context) ProxyConfig {
 
 func FileServer(ctx *cli.Context) {
 	port := ctx.String(portFlag.Name)
-	rest := restful.InitRestService(port)
-	err := rest.Start()
+	if !(ctx.IsSet(clusterUrlFlag.Name) && ctx.IsSet(clusterName.Name) && ctx.IsSet(clusterPass.Name) && ctx.IsSet(headlessUrl.Name)) {
+		log.Fatal("cluster url unset")
+	}
+	clusterUrl := ctx.String(clusterUrlFlag.Name)
+	clusterNameStr := ctx.String(clusterName.Name)
+	clusterPassStr := ctx.String(clusterPass.Name)
+	chromeUrl := ctx.String(headlessUrl.Name)
+
+	//init chrome agent
+	agent := snapshot.NewHeadlessAgent(chromeUrl)
+	agent.Start()
+	//init ipfs cluster client
+	client, err := cluster_client.NewClusterClient(clusterUrl, clusterNameStr, clusterPassStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//init rpc server
+	rest := restful.InitRestService(port, client, agent)
+	err = rest.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
