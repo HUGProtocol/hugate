e9358d806cc62a34d3f767333d316842aa7d16a0
CaleHH <caleihhmy@gmail.com>
2022-11-16 22:59:44 +0800

    modity ipfs url

--- restful/src/models.rs
+++ restful/src/models.rs
@@ -1,6 +1,12 @@
+pub mod comments;
 pub mod likes;
 pub mod pagination;
+pub mod pass;
 pub mod thoughts;
 pub mod users;
-pub mod comments;
-pub mod pass;
+
+pub fn ReplaceIPFSUrl(url: String) -> String {
+    url.clone()
+        .split_once("/ipfs/")
+        .map_or(url, |(_, y)| "https://hug.land/ipfs/".to_string() + y)
+}
